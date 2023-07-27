94498ba6e92e07b3e8569d852e7d78cc6260ca5d
alan14112003 <sonnn.21it@gmil.com>
2023-07-27 08:47:43 +0700

    add origin * to cors

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { ExpressPeerServer } = require("peer");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res, next) =>
   res.send("Chào mọi người, đây là server peer của Alan!")
